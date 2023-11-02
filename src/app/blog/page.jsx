88d6eac5746728093f867d0a90321fda9193c69e
Kem-consolation <consolationlotachi@gmail.com>
2023-11-02 15:12:55 +0100

    added darkmode

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,8 +1,43 @@
 import React from "react";
 import styles from "./page.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 const Blog = () => {
-  return <div className={styles.contact}>Blog</div>;
+  return (
+    <div className={styles.mainContainer}>
+      <Link href="blog/testId" className={styles.container}>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.image}
+            src=""
+            alt=""
+            height={400}
+            width={200}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+      <Link href="blog/testId" className={styles.container}>
+        <div className={styles.imgContainer}>
+          <Image
+            className={styles.image}
+            src=""
+            alt=""
+            height={400}
+            width={200}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Blog;
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import styles from "./page.module.css";
+import Button from "@/components/Button/Button";
+import Image from "next/image";
 
-const category = () => {
-  return <div>category</div>;
+const Category = ({ params }) => {
+  console.log(params);
+  return (
+    <div>
+      <h1 className={styles.catTitle}>{params.category}</h1>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image className={styles.img} fill={true} src="" alt="" />
+        </div>
+      </div>
+      <div className={styles.item}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <Button text="See More" url="#" />
+        </div>
+        <div className={styles.imgContainer}>
+          <Image className={styles.img} fill={true} src="" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default category;
+export default Category;
